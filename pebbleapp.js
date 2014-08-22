acdc487eb3b61aa233b5ccd7617ec09ec880f006
Andreas Kleve <andreas.kleve@happiness.se>
2014-08-22 23:06:56 +0200

    Commit.

--- pebbleapp.js
+++ pebbleapp.js
@@ -1,5 +1,7 @@
 (function($) {
 
+  simply.title('Hello World!');
+  
 	ajax({ url: 'http://www.arvsfonden.se/' }, function(data){
 	  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
 	  simply.title(headline);
