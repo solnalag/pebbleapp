585b9b229c1b0c9915f1bc81d8713e39537cbb5d
Andreas Kleve <andreas.kleve@happiness.se>
2014-08-23 15:47:43 +0200

    Commit.

--- pebbleapp.js
+++ pebbleapp.js
@@ -1,19 +1,21 @@
 /**
  *
  */
-simply.title('Hello World!');
+simply.title('Busstidtabell');
 
-ajax({ url: 'http://www.arvsfonden.se/' }, function(data){
-	var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-	simply.subtitle(headline);
+ajax({url: 'http://sl.se/#/Travel/SearchTravelById/Slussen%20(Stockholm)/Brunn%20(V%C3%A4rmd%C3%B6)/9192/4300/null/depart/sv/null/null/null/null/null/null/null/null/false/null/0' }, function(data){
+	var slussenId = document.getElementById('searchResults');
+	var slussen = slussenId.match(/<h2>(.*?)<\/h2>/)[1];
+	var strippedSlussen = slussen.replace(/(<([^>]+)>)/ig,"");
+	simply.subtitle(strippedSlussen);
 });
 
 simply.on('singleClick', function(e) {
   if (e.button === 'up') {
-    simply.subtitle('Upp');
+    simply.title('Brunn => Slussen');
     simply.vibe('short');
   } else if (e.button === 'down') {
-    simply.subtitle('Ner');
-    simply.vibe('long');
+    simply.title('Slussen => Brunn');
+    simply.vibe('short');
   }
 });
