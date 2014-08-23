28b00a0c2fa66852c2948cc895fdb0b33aa383aa
Andreas Kleve <andreas.kleve@happiness.se>
2014-08-23 15:54:57 +0200

    Commit.

--- pebbleapp.js
+++ pebbleapp.js
@@ -6,7 +6,8 @@ simply.title('Busstidtabell');
 ajax({url: 'http://sl.se/#/Travel/SearchTravelById/Slussen%20(Stockholm)/Brunn%20(V%C3%A4rmd%C3%B6)/9192/4300/null/depart/sv/null/null/null/null/null/null/null/null/false/null/0' }, function(data){
 	console.log(data);
 	var slussenId = document.getElementById('searchResults');
-	var slussen = data.match(/<h2>(.*?)<\/h2>/)[1];
+	var slussen = data.match(/<div class="result-info clickable" data-ng-class="{active: getClicked($index)}" data-ng-click="setClicked($index);" data-ng-init="reset()" tabindex="0">
+                    <h2>(.*?)<\/h2>/)[1];
 	var strippedSlussen = slussen.replace(/(<([^>]+)>)/ig,"");
 	simply.subtitle(strippedSlussen);
 });
