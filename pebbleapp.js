6691db04240ed6b0ff1f65288e9d6f3cc3907605
Andreas Kleve <andreas.kleve@happiness.se>
2014-08-27 09:30:33 +0200

    Added new script.

--- pebbleapp.js
+++ pebbleapp.js
@@ -21,62 +21,62 @@ simply.on('singleClick', function(e) {
   
     url = 'https://api.trafiklab.se/sl/realtid/GetDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=9192';
     
-  ajax({ url: url, type: 'json' }, function(data) {
-    simply.text({
-      title: data.Departure.Metros.Metro[0].StationName,
-       subtitle: data.Departure.Metros.Metro[0].DisplayRow1,
-       body: data.Departure.Metros.Metro[0].DisplayRow2,
-    });
-    
-});
-    
-  } else {
-url = 'https://api.trafiklab.se/sl/realtid/GetDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=4300';
-    
-ajax({ url: url, type: 'json' }, function(data) {
-  simply.text({
-    title: 'Test' + data.Departure.Metros.Metro[1].StationName,
-     subtitle: data.Departure.Metros.Metro[1].DisplayRow1,
-     body: data.Departure.Metros.Metro[1].DisplayRow2,
-    });
-});
-  }
-});
-
-simply.on('longClick', function(e) {
-  if (e.button === 'up') {
-    url2 = 'https://api.trafiklab.se/sl/realtid/GetDpsDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=[id]&timeWindow=10';
-        
-    ajax({ url: url2, type: 'json' }, function(data) {
+    ajax({ url: url, type: 'json' }, function(data) {
       simply.text({
-        title: data.DPS.Trams.DpsTram[1].StopAreaName,
-        subtitle: data.DPS.Trams.DpsTram[1].Destination + ' ' + data.DPS.Trams.DpsTram[1].DisplayTime,
-       body: data.DPS.Trams.DpsTram[0].Destination + ' ' + data.DPS.Trams.DpsTram[0].DisplayTime
-        });
+        title: data.Departure.Metros.Metro[0].StationName,
+         subtitle: data.Departure.Metros.Metro[0].DisplayRow1,
+         body: data.Departure.Metros.Metro[0].DisplayRow2,
+      });
+      
     });
-
-  } else if (e.button === 'down') {
-  
-    url2 = 'https://api.trafiklab.se/sl/realtid/GetDpsDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=[id]&timeWindow=10';
-    
-  ajax({ url: url2, type: 'json' }, function(data) {
-    simply.text({
-      title: data.DPS.Trams.DpsTram[1].StopAreaName,
-      subtitle: data.DPS.Trams.DpsTram[0].Destination + ' ' + data.DPS.Trams.DpsTram[0].DisplayTime,
-       body: data.DPS.Trams.DpsTram[1].Destination + ' ' + data.DPS.Trams.DpsTram[1].DisplayTime
-    });
-    
-});
-    
   }
+//   else {
+// url = 'https://api.trafiklab.se/sl/realtid/GetDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=4300';
+    
+// ajax({ url: url, type: 'json' }, function(data) {
+//   simply.text({
+//     title: 'Test' + data.Departure.Metros.Metro[1].StationName,
+//      subtitle: data.Departure.Metros.Metro[1].DisplayRow1,
+//      body: data.Departure.Metros.Metro[1].DisplayRow2,
+//     });
+// });
+//   }
 });
 
-url = 'https://api.trafiklab.se/sl/realtid/GetDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=[id]';
+// simply.on('longClick', function(e) {
+//   if (e.button === 'up') {
+//     url2 = 'https://api.trafiklab.se/sl/realtid/GetDpsDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=[id]&timeWindow=10';
+        
+//     ajax({ url: url2, type: 'json' }, function(data) {
+//       simply.text({
+//         title: data.DPS.Trams.DpsTram[1].StopAreaName,
+//         subtitle: data.DPS.Trams.DpsTram[1].Destination + ' ' + data.DPS.Trams.DpsTram[1].DisplayTime,
+//        body: data.DPS.Trams.DpsTram[0].Destination + ' ' + data.DPS.Trams.DpsTram[0].DisplayTime
+//         });
+//     });
+
+//   } else if (e.button === 'down') {
+  
+//     url2 = 'https://api.trafiklab.se/sl/realtid/GetDpsDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=[id]&timeWindow=10';
     
-ajax({ url: url, type: 'json' }, function(data) {
-  simply.text({
-    title: data.Departure.Metros.Metro[1].StationName,
-     subtitle: data.Departure.Metros.Metro[1].DisplayRow1,
-     body: data.Departure.Metros.Metro[1].DisplayRow2
-    });
+//   ajax({ url: url2, type: 'json' }, function(data) {
+//     simply.text({
+//       title: data.DPS.Trams.DpsTram[1].StopAreaName,
+//       subtitle: data.DPS.Trams.DpsTram[0].Destination + ' ' + data.DPS.Trams.DpsTram[0].DisplayTime,
+//        body: data.DPS.Trams.DpsTram[1].Destination + ' ' + data.DPS.Trams.DpsTram[1].DisplayTime
+//     });
+    
+// });
+    
+//   }
+// });
+
+// url = 'https://api.trafiklab.se/sl/realtid/GetDepartures.json?key=SLukjHHJSY0z6E8tF4RJJYiSkHgIrC0J&siteId=[id]';
+    
+// ajax({ url: url, type: 'json' }, function(data) {
+//   simply.text({
+//     title: data.Departure.Metros.Metro[1].StationName,
+//      subtitle: data.Departure.Metros.Metro[1].DisplayRow1,
+//      body: data.Departure.Metros.Metro[1].DisplayRow2
+//     });
 });
