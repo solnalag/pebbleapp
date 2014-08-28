2f39d52e493cff4d4fa34becd030049bf30df21f
Andreas Kleve <andreas.kleve@happiness.se>
2014-08-28 07:45:11 +0200

    Testing.

--- pebbleapp.js
+++ pebbleapp.js
@@ -22,7 +22,7 @@ simply.on('singleClick', function(e) {
         
     ajax({ url: url, type: 'json' }, function(data) {
       simply.text({
-        title: 'Från ' + data.HafasResponse.Trip[0].SubTrip.Origin.#text,
+        title: 'Från ' + data.HafasResponse.Trip[0].Summary.Origin.#text,
         subtitle: data.HafasResponse.Trip[0].SubTrip.Transport.Name,
         body: data.HafasResponse.Trip[0].SubTrip.Transport.Name,
       });
